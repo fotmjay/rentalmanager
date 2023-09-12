e08b12aeb38125face574d71db94ad6a6bd810ee
unknown <95885719+fotmjay@users.noreply.github.com>
2023-09-12 14:08:24 -0400

    modified warning banner

--- src/components/WarningBanner.jsx
+++ src/components/WarningBanner.jsx
@@ -2,8 +2,8 @@ export default function WarningBanner() {
   return (
     <div className="warningBanner">
       <h4 className="warningBannerText">
-        Due to hosting plans, server will sometime shut down which will cause a disconnection and might log you out.
-        Please log back in.
+        Due to hosting plans, server regularly shuts down which will cause a disconnection. Might also take a while to
+        load when it first spins up.
       </h4>
     </div>
   );
